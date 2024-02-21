045393c2e72b60e662a1adcdd893d8a18304fb88
ImanBanerjee <imanbanerjee03@gmail.com>
2024-02-21 17:47:30 +0530

    71A codforces solution

--- 71A.py
+++ 71A.py
@@ -1,11 +1,19 @@
 while True:
-    word = input("Enter a word (or type 'exit' to close the program): ")
+    try:
+        word = input("Enter a word (or type 'exit' to close the program): ")
 
-    if word.lower() == 'exit':
-        break
+        if word.lower() == 'exit':
+            break
 
-    if word.isalpha():
-        if len(word) > 10:
-            print(word[0] + str(len(word) - 2) + word[-1])
+        if word.isalpha():
+            if len(word) > 10:
+                print(word[0] + str(len(word) - 2) + word[-1])
+            else:
+                print(word)
         else:
-            print(word)
+            print("Please enter a valid word (only alphabetic characters allowed).")
+    except EOFError:
+        print("Error: EOF (End of File) encountered. Exiting the program.")
+        break
+    except Exception as e:
+        print("An error occurred:", e)
